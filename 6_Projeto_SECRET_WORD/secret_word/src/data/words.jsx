482b8452982bee6d7bac04df9a2cd5a21b87b2fb
rafavanzele <rafa_vanzele@hotmail.com>
2024-02-07 16:58:48 -0200

    primeiros passos projeto palavra secreta

---
+++ 6_Projeto_SECRET_WORD/secret_word/src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordList = {
+    carro: ['Motor', 'Porta', 'Retrovisor', 'Embreagem', 'Banco'],
+    fruta: ['Caqui', 'Jabuticaba', 'Cacau', 'Melancia', 'Açaí'],
+    cep: ['Argentina', 'Botucatu', 'Joinville', 'Escócia', 'México'],
+    cor: ['Azul', 'Dourado', 'Roxo', 'Laranja', 'Verde'],
+    comida: ['Arroz', 'Lasanha', 'Strogonoff', 'Ensopado', 'Macarronada'],
+    programação: ['Linguagem', 'Framework', 'Javascript', 'React', 'Backend'],
+}
