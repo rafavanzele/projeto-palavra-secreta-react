b0fc013beba3a0482b6dfd68081c7703f10b0ae4
rafavanzele <rafa_vanzele@hotmail.com>
2024-02-08 17:18:40 -0200

    avançando na criação do jogo (components)

--- 6_Projeto_SECRET_WORD/secret_word/src/components/StartScreen.jsx
+++ 6_Projeto_SECRET_WORD/secret_word/src/components/StartScreen.jsx
@@ -1,11 +1,11 @@
 import './StartScreen.css'
 
-const StartScreen = () => {
+const StartScreen = ({startGame}) => {
   return (
     <div className='start'>
         <h1>Palavra Secreta</h1>
         <p>Clique abaixo para começar o jogo</p>
-        <button>Clique Aqui!</button>
+        <button onClick={startGame}>Começar o jogo</button>
     </div>
   )
 }
